298de167651098916c39661eb3daf7162a4ed1a6
swsk33 <yanhanhui2001a@163.com>
2021-05-31 23:36:37 +0800

    v2.0.0

---
+++ EVTools/EditForm.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace EVTools
+{
+	partial class EditForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EditForm));
+			this.editValue = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// editValue
+			// 
+			this.editValue.Location = new System.Drawing.Point(45, 54);
+			this.editValue.Name = "editValue";
+			this.editValue.Size = new System.Drawing.Size(336, 25);
+			this.editValue.TabIndex = 0;
+			// 
+			// EditForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(426, 159);
+			this.Controls.Add(this.editValue);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "EditForm";
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "编辑该条目";
+			this.TopMost = true;
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox editValue;
+	}
+}
